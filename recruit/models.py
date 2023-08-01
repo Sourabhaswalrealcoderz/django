1f5970cd0b20928a221c4656662990d983fea259
sourabh <sourabhaswal98@gmail.com>
2023-08-01 11:25:26 +0530

    recruit_master

--- recruit/models.py
+++ recruit/models.py
@@ -7,3 +7,13 @@ class recruit(models.Model):
     studentlocation= models.CharField(max_length=1000,default="description")
     def __str__(self):
         return self.name
+    
+
+
+
+class recruit_user_master(models.Model):
+    usernamename = models.CharField(max_length=100)
+    useraddressaddess= models.CharField(max_length=1000,default="description")
+    userlocationlocation= models.CharField(max_length=1000,default="description")
+    def __str__(self):
+        return self.name    
